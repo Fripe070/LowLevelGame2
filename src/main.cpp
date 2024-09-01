665fb68e8c313c6875294776adf5f0380c333180
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-09-01 18:59:34 +0200

    Equilateral

--- src/main.cpp
+++ src/main.cpp
@@ -128,8 +128,8 @@ int main(int, char *[])
     glUseProgram(programID);
 
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
+        -1./3, -0.5f, 0.0f,
+         1./3, -0.5f, 0.0f,
          0.0f,  0.5f, 0.0f,
     };
     GLuint vao;
