4afcb41299f0ee3d05c2dc99d62b9dde0c2dfbc5
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-09-01 18:54:47 +0200

    OpenGL triangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,87 @@
 #include "SDL.h"
 #include <gl/glew.h>
 #include <SDL_opengl.h>
-#include <gl/glu.h>
 #include <config.hpp>
+#include <string>
 
-void drawTestGeo() {
-    static float g_rotation_angle = -0.5f;
-    g_rotation_angle += 0.5f;
-    glRotatef(g_rotation_angle, 1.0f, 1.0f, 1.0f);
 
-    glBegin(GL_QUADS);
 
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-    glVertex3f(1.0f, 1.0f, 1.0f);
-    glVertex3f(-1.0f, 1.0f, 1.0f);
+std::string getShaderInfoLog(GLuint shaderID) {
+    GLint infoLogLength;
+    glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+    std::string infoLog(infoLogLength, '\0');
+    glGetShaderInfoLog(shaderID, infoLogLength, nullptr, infoLog.data());
+    return infoLog;
+}
 
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glVertex3f(1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, -1.0f);
-
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, -1.0f);
-    glVertex3f(1.0f, 1.0f, -1.0f);
-    glVertex3f(1.0f, 1.0f, 1.0f);
-    glVertex3f(1.0f, -1.0f, 1.0f);
-
-    glColor3f(1.0f, 1.0f, 0.0f);
-    glVertex3f(-1.0f, -1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, -1.0f);
-    glVertex3f(-1.0f, 1.0f, 1.0f);
-    glVertex3f(-1.0f, -1.0f, 1.0f);
-
-    glEnd();
+std::string getProgramInfoLog(GLuint programID) {
+    GLint infoLogLength;
+    glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+    std::string infoLog(infoLogLength, '\0');
+    glGetProgramInfoLog(programID, infoLogLength, nullptr, infoLog.data());
+    return infoLog;
 }
 
 
-int initGL(SDL_Window *window) {
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+bool initGlProgram(const GLuint &programID) {
+    GLuint vertShaderID = glCreateShader( GL_VERTEX_SHADER );
+    GLuint fragShaderID = glCreateShader( GL_FRAGMENT_SHADER );
 
-    SDL_GLContext context = SDL_GL_CreateContext(window);
-    if (!context) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create OpenGL context: %s", SDL_GetError());
-        return -1;
+    const auto vertShaderSrc = R"(
+        #version 330 core
+        layout (location = 0) in vec3 aPos;
+
+        void main() {
+            gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        }
+    )";
+    const auto vertShaderSrcLen = static_cast<GLint>(strlen(vertShaderSrc));
+    glShaderSource(vertShaderID, 1, &vertShaderSrc, &vertShaderSrcLen);
+    glCompileShader(vertShaderID);
+
+    GLint vertResult = GL_FALSE;
+    glGetShaderiv(vertShaderID, GL_COMPILE_STATUS, &vertResult);
+    if (vertResult == GL_FALSE) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Vertex shader compilation failed: %s", getShaderInfoLog(vertShaderID).c_str());
+        return false;
     }
-    SDL_GL_MakeCurrent(window, context);
 
-    GLenum glewError = glewInit();
-    if (glewError != GLEW_OK) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize GLEW: %s", glewGetErrorString(glewError));
-        return -1;
+    auto fragShaderSrc = R"(
+        #version 330 core
+        out vec4 FragColor;
+
+        void main() {
+            FragColor = vec4(1, 1, 1, 1);
+        }
+    )";
+    const auto fragShaderSrcLen = static_cast<GLint>(strlen(vertShaderSrc));
+    glShaderSource(fragShaderID, 1, &fragShaderSrc, &fragShaderSrcLen);
+    glCompileShader(fragShaderID);
+
+    GLint fragResult = GL_FALSE;
+    glGetShaderiv(fragShaderID, GL_COMPILE_STATUS, &fragResult);
+    if (fragResult == GL_FALSE) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Fragment shader compilation failed: %s", getShaderInfoLog(fragShaderID).c_str());
+        return false;
     }
 
-    return 0;
+    GLint progResult = GL_FALSE;
+    glAttachShader(programID, vertShaderID);
+    glAttachShader(programID, fragShaderID);
+    glLinkProgram(programID);
+
+    glGetProgramiv(programID, GL_LINK_STATUS, &progResult);
+    if (progResult == GL_FALSE) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Program linking failed: %s", getProgramInfoLog(programID).c_str());
+        return false;
+    }
+
+    glDeleteShader(vertShaderID);
+    glDeleteShader(fragShaderID);
+
+    return true;
 }
+
 
 int main(int, char *[])
 {
@@ -66,6 +89,10 @@ int main(int, char *[])
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
         return 1;
     }
+    // Modern OpenGL with core profile
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
     SDL_Window *window = SDL_CreateWindow(
         "LowLevelGame attempt 2 (million)",
@@ -73,28 +100,48 @@ int main(int, char *[])
         SDL_WINDOWPOS_UNDEFINED,
         config::window_size[0],
         config::window_size[1],
-        SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL
+        SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE
     );
     if (!window) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create window: %s", SDL_GetError());
         return 1;
     }
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(
-        window,
-        -1,
-        SDL_RENDERER_ACCELERATED
-    );
-    if (!renderer) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create renderer: %s", SDL_GetError());
-        return 1;
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (!glContext) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create OpenGL context: %s", SDL_GetError());
+        return -1;
+    }
+    SDL_GL_MakeCurrent(window, glContext);
+
+    GLenum glewError = glewInit();
+    if (glewError != GLEW_OK) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize GLEW: %s", glewGetErrorString(glewError));
+        return -1;
     }
 
-    if (0 != initGL(window)) {
-        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize OpenGL: %s", SDL_GetError());
-        return 1;
+    GLuint programID = glCreateProgram();
+    if (!initGlProgram(programID)) {
+        SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize OpenGL");
+        return -1;
     }
+    glUseProgram(programID);
 
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+    };
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+    glEnableVertexAttribArray(0);
 
     SDL_Event event;
     while (true) {
@@ -103,30 +150,16 @@ int main(int, char *[])
             break;
         }
 
-        // TODO: (Fripe) actually look at this nonsense rather than just copy-paste it from the old project, there are probably some bad choices.
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        const float aspectRatio = static_cast<float>(config::window_size[0]) / static_cast<float>(config::window_size[1]);
-        const float top = 0.1f * std::tan(45.0f * 0.5f * 3.14159265f / 180.0f);
-        const float bottom = -top;
-        const float right = top * aspectRatio;
-        const float left = -right;
-        glFrustum(left, right, bottom, top, 0.1f, 100.0f);
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
-        glTranslatef(0.0f, 0.0f, -5.0f);
-        glEnable(GL_DEPTH_TEST);
-        drawTestGeo();
+        glClear(GL_COLOR_BUFFER_BIT);
 
-        SDL_SetRenderDrawColor(renderer, 24, 27, 32, 255);
-        SDL_RenderClear(renderer);
-        // TODO: (Fripe) Which one is the "correct" one?
-        // SDL_RenderPresent(renderer);
+        glUseProgram(programID);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         SDL_GL_SwapWindow(window);
     }
 
-    SDL_DestroyRenderer(renderer);
+    SDL_GL_DeleteContext(glContext);
     SDL_DestroyWindow(window);
 
     SDL_Quit();
