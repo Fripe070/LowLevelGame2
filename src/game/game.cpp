fe44e660eac37d566b482b84a47a5a9ada0a05da
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2024-11-09 19:06:22 +0100

    Change how state is stored

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -9,88 +9,105 @@
 
 #include "camera.h"
 
-std::unique_ptr<Engine::Loader::Model> model;
-std::unique_ptr<Engine::ShaderProgram> shader;
-std::unique_ptr<Engine::Manager::TextureManager> textureManager;
 
-Camera camera;
-CameraController cController = CameraController(camera, 0.1);
+struct PlayerState {
+    Camera camera;
+    CameraController cController = CameraController(camera, 0.1);
+};
 
+
+struct LevelState {
+    std::vector<Engine::Loader::Model> models;
+    std::vector<Engine::ShaderProgram> shaders;
+    Engine::Manager::TextureManager textureManager;
+
+    PlayerState player;
+};
+
+struct GameState {
+    LevelState level;
+};
+
+std::unique_ptr<GameState> gameState;
+
+#define LEVEL gameState->level
+#define PLAYER LEVEL.player
+#define CAMERA PLAYER.camera
 
 bool setupGame() {
-    model = std::make_unique<Engine::Loader::Model>("resources/map.obj");
-    shader = std::make_unique<Engine::ShaderProgram>("resources/vert.vert", "resources/frag.frag");
-    textureManager = std::make_unique<Engine::Manager::TextureManager>();
+    gameState = std::make_unique<GameState>();
+
+    LEVEL.models.emplace_back("resources/map.obj");
+    LEVEL.shaders.emplace_back("resources/vert.vert", "resources/frag.frag");
 
     SDL_SetRelativeMouseMode(SDL_TRUE);
     glEnable(GL_DEPTH_TEST);
     return true;
 }
 void shutdownGame() {
-    model.reset();
-    shader.reset();
-    textureManager.reset();
+    gameState.reset();
 }
+
 
 bool renderUpdate(const double deltaTime, const WindowSize &windowSize) {
     const Uint8* keyState = SDL_GetKeyboardState(nullptr);
     auto inputDir = glm::vec3(0.0f, 0.0f,  0.0f);
     if (keyState[SDL_SCANCODE_W])
-        inputDir += camera.forward();
+        inputDir += CAMERA.forward();
     if (keyState[SDL_SCANCODE_S])
-        inputDir -= camera.forward();
+        inputDir -= CAMERA.forward();
     if (keyState[SDL_SCANCODE_A])
-        inputDir -= glm::normalize(glm::cross(camera.forward(), camera.up()));
+        inputDir -= glm::normalize(glm::cross(CAMERA.forward(), CAMERA.up()));
     if (keyState[SDL_SCANCODE_D])
-        inputDir += glm::normalize(glm::cross(camera.forward(), camera.up()));
+        inputDir += glm::normalize(glm::cross(CAMERA.forward(), CAMERA.up()));
     if (keyState[SDL_SCANCODE_SPACE])
-        inputDir += camera.up();
+        inputDir += CAMERA.up();
     if (keyState[SDL_SCANCODE_LSHIFT])
-        inputDir -= camera.up();
+        inputDir -= CAMERA.up();
     if (keyState[SDL_SCANCODE_ESCAPE])
         SDL_SetRelativeMouseMode(SDL_FALSE);
 
     inputDir = glm::dot(inputDir, inputDir) > 0.0f ? glm::normalize(inputDir) : inputDir; // dot(v, v) is squared length
     constexpr auto CAMERA_SPEED = 2.5f;
-    camera.position += inputDir * CAMERA_SPEED * static_cast<float>(deltaTime);
+    CAMERA.position += inputDir * CAMERA_SPEED * static_cast<float>(deltaTime);
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    shader->use();
+    LEVEL.shaders[0].use();
 
-    shader->setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
-    shader->setVec3("dirLight.ambient", 0.5f, 0.5f, 0.5f);
-    shader->setVec3("pointLights.diffuse", 0.4f, 0.4f, 0.4f);
-    shader->setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
+    LEVEL.shaders[0].setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
+    LEVEL.shaders[0].setVec3("dirLight.ambient", 0.5f, 0.5f, 0.5f);
+    LEVEL.shaders[0].setVec3("pointLights.diffuse", 0.4f, 0.4f, 0.4f);
+    LEVEL.shaders[0].setVec3("dirLight.specular", 0.5f, 0.5f, 0.5f);
 
-    shader->setVec3("pointLights[0].position", 1.2f, 1.0f, 2.0f);
-    shader->setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
-    shader->setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
-    shader->setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
-    shader->setFloat("pointLights[0].constant", 1.0f);
-    shader->setFloat("pointLights[0].linear", 0.09f);
-    shader->setFloat("pointLights[0].quadratic", 0.032f);
+    LEVEL.shaders[0].setVec3("pointLights[0].position", 1.2f, 1.0f, 2.0f);
+    LEVEL.shaders[0].setVec3("pointLights[0].ambient", 0.05f, 0.05f, 0.05f);
+    LEVEL.shaders[0].setVec3("pointLights[0].diffuse", 0.8f, 0.8f, 0.8f);
+    LEVEL.shaders[0].setVec3("pointLights[0].specular", 1.0f, 1.0f, 1.0f);
+    LEVEL.shaders[0].setFloat("pointLights[0].constant", 1.0f);
+    LEVEL.shaders[0].setFloat("pointLights[0].linear", 0.09f);
+    LEVEL.shaders[0].setFloat("pointLights[0].quadratic", 0.032f);
 
-    shader->setVec3("spotLight.position", camera.position);
-    shader->setVec3("spotLight.direction", camera.forward());
-    shader->setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-    shader->setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
-    shader->setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
-    shader->setFloat("spotLight.constant", 1.0f);
-    shader->setFloat("spotLight.linear", 0.09f);
-    shader->setFloat("spotLight.quadratic", 0.032f);
-    shader->setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
-    shader->setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
+    LEVEL.shaders[0].setVec3("spotLight.position", CAMERA.position);
+    LEVEL.shaders[0].setVec3("spotLight.direction", CAMERA.forward());
+    LEVEL.shaders[0].setVec3("spotLight.ambient", 0.0f, 0.0f, 0.0f);
+    LEVEL.shaders[0].setVec3("spotLight.diffuse", 1.0f, 1.0f, 1.0f);
+    LEVEL.shaders[0].setVec3("spotLight.specular", 1.0f, 1.0f, 1.0f);
+    LEVEL.shaders[0].setFloat("spotLight.constant", 1.0f);
+    LEVEL.shaders[0].setFloat("spotLight.linear", 0.09f);
+    LEVEL.shaders[0].setFloat("spotLight.quadratic", 0.032f);
+    LEVEL.shaders[0].setFloat("spotLight.cutOff", glm::cos(glm::radians(12.5f)));
+    LEVEL.shaders[0].setFloat("spotLight.outerCutOff", glm::cos(glm::radians(15.0f)));
 
-    shader->setFloat("material.shininess", 32.0f);
+    LEVEL.shaders[0].setFloat("material.shininess", 32.0f);
 
-    shader->setVec3("viewPos", camera.position);
-    shader->setMat4("projection", camera.getProjectionMatrix(static_cast<float>(windowSize.width) / windowSize.height));
-    shader->setMat4("view", camera.getViewMatrix());
-    shader->setMat4("model", glm::mat4(1.0f));
+    LEVEL.shaders[0].setVec3("viewPos", CAMERA.position);
+    LEVEL.shaders[0].setMat4("projection", CAMERA.getProjectionMatrix(static_cast<float>(windowSize.width) / windowSize.height));
+    LEVEL.shaders[0].setMat4("view", CAMERA.getViewMatrix());
+    LEVEL.shaders[0].setMat4("model", glm::mat4(1.0f));
 
-
-    model->Draw(*textureManager, *shader);
+    for (auto &model : LEVEL.models)
+        model.Draw(LEVEL.textureManager, LEVEL.shaders[0]);
     return true;
 }
 bool physicsUpdate(const double deltaTime) {
@@ -101,10 +118,10 @@ bool handleEvent(const SDL_Event &event) {
     switch (event.type) {
         default: break;
         case SDL_MOUSEWHEEL:
-            cController.zoom(event.wheel.y);
+            PLAYER.cController.zoom(event.wheel.y);
             break;
         case SDL_MOUSEMOTION:
-            cController.look(event.motion);
+            PLAYER.cController.look(event.motion);
             break;
     }
 
