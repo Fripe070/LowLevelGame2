6e672bbab0fb16b7f44532d86608ca88d1961a26
Fripe070 <72686066+Fripe070@users.noreply.github.com>
2025-05-04 01:23:17 +0200

    Forward error in powershell build scripts

--- debug.ps1
+++ debug.ps1
@@ -1,2 +1,3 @@
 meson setup build/debug
 meson compile -C build/debug
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
--- release.ps1
+++ release.ps1
@@ -1,2 +1,3 @@
 meson setup build/release --buildtype release --optimization=3
 meson compile -C build/release
+if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
